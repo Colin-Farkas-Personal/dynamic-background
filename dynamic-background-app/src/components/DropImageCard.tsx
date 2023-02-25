d6deaaa5506c123d1af7e97731fd9c14e41a7157
Colin Farkas <colin.farkas@walley.se>
2023-02-25 22:33:43 +0100

    Added initial styling for Home

--- dynamic-background-app/src/components/DropImageCard.tsx
+++ dynamic-background-app/src/components/DropImageCard.tsx
@@ -1,12 +1,18 @@
 import * as React from 'react';
 import { Component } from 'react';
+import file from '@/assets/icons/file.svg';
 
 import styles from './DropImageCard.module.scss'
+import classNames from 'classnames';
 
 function DropImageCard() {
-    return ( <div className={styles['card-container']}>
-        <h2>Drop you image below</h2>
-    </div> );
+    return (
+        <div className={styles.card}>
+            <p className={classNames(styles['card__text'])}>Drop your image here</p>
+            <img src={file} alt="file" className={classNames(styles['card__icon'])}/>
+            <button type='button' className={classNames(styles['card__button'])}>Select a file</button>
+        </div> 
+    );
 }
 
 export default DropImageCard;
---
+++ dynamic-background-app/src/pages/generated.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from "react";
+
+interface GeneratedBackgroundProps {
+    imageFile: String,
+}
+ 
+const GeneratedBackground: FunctionComponent<GeneratedBackgroundProps> = () => {
+    return ( <div>
+        <h1>AWESOME BACKGROUND</h1>
+    </div> );
+}
+ 
+export default GeneratedBackground;
