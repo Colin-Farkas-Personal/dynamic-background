8861d0ed5567c92bd9307851adcea31c5804db70
Colin Farkas <colin.farkas@walley.se>
2023-03-09 23:25:47 +0100

    Minor fixes to card drop field

--- dynamic-background-app/src/components/DropImageCard.tsx
+++ dynamic-background-app/src/components/DropImageCard.tsx
@@ -9,10 +9,14 @@ import file from '@/assets/icons/file.svg';
 import { getLinearGradientStyle, getBoxShadowStyle } from '@/helpers/getGradient';
 import styles from './DropImageCard.module.scss'
 import classNames from 'classnames';
+import Icon from '@/helpers/Icon';
 
 function DropImageCard() {
+    // HOOKS
+    const [isDrageEnterStyle, setIsDrageEnterStyle] = useState(false);
     const fileInputRef = useRef<HTMLInputElement>(null);
 
+    // FUNCTIONS
     const handleHover = (e: MouseEvent<HTMLFormElement, globalThis.MouseEvent>) => {
         e.preventDefault();
         
@@ -32,32 +36,43 @@ function DropImageCard() {
 
     const handleDragEnter = (e: DragEvent<HTMLFormElement>) => {
         e.preventDefault();
-
-        alert("Hey!");
+        setIsDrageEnterStyle(true);
+    }
+    const handleDragLeave = (e: DragEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setIsDrageEnterStyle(false);    
     }
 
     const handleButtonClick = () => {
         fileInputRef.current?.click()
     };
 
+    // JSX
     return (
         <form 
-            className={styles.card}
-            onDragEnter={(e) => handleDragEnter(e)}
-            onSubmit={(e) => e.preventDefault()}
+            className={classNames(
+                styles['card'], 
+                {[styles['card--onDrageEnter']] : isDrageEnterStyle }
+            )}
             onMouseEnter={(e) => handleHover(e)} 
             onMouseLeave={(e) => handleLeave(e)}
+            onDragEnter={(e) => handleDragEnter(e)}
+            onDragLeave={(e) => handleDragEnter(e)}
+            onSubmit={(e) => e.preventDefault()}
         >
             <input
                 id='file-upload'
                 ref={fileInputRef}
                 type="file"
             />
+            <div 
+                className={classNames(styles['card__header'])}
+                onClick={e => e.stopPropagation()}
+            >
+                <span className={classNames(styles['card__text'])}>Drop your image here</span>
+                <Icon />
+            </div>
             <label htmlFor="file-upload">
-                <div className={classNames(styles['card__header'])}>
-                    <span className={classNames(styles['card__text'])}>Drop your image here</span>
-                    <img src={file} alt="file" className={classNames(styles['card__icon'])}/>
-                </div>
                 <button type='button' className={classNames(styles['card__button'])} onClick={handleButtonClick}>Select a file</button>
             </label>
         </form>
